9ecb8e0bcfb8a6c3e663ed7b1edb4174acfba5f8
Daniel Cintra <danic10@gmail.com>
2023-12-29 11:37:29 -0300

    Align image not available icon.

--- resources/js/Components/Misc/AppImageNotAvailable.vue
+++ resources/js/Components/Misc/AppImageNotAvailable.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="ml-3 flex h-10 w-10 items-center justify-center rounded bg-gradient-to-bl from-skin-neutral-3 to-skin-neutral-6"
+        class="flex h-10 w-10 items-center justify-center rounded bg-gradient-to-bl from-skin-neutral-3 to-skin-neutral-6"
     >
         <span class="text-xs text-skin-neutral-9">N/A</span>
     </div>
